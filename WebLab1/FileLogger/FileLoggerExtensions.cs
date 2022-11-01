7e863977158d2a39b86ea06463b9dfbbac4c9447
Dark Spark <vadimlisovoi@gmail.com>
2022-11-02 01:06:48 +0200

    lab23

---
+++ WebLab1/FileLogger/FileLoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace WebLab1.FileLogger
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory, string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
--- WebLab1/Startup.cs
+++ WebLab1/Startup.cs
@@ -4,7 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebLab1.FileLogger;
+using WebLab1.Services.Helpers;
+using WebLab1.Services.Interfaces;
+using WebLab1.Services.Services;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,11 +30,17 @@ namespace WebLab1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<MailSettings>(Configuration.GetSection("EmailSettings"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            System.IO.Directory.CreateDirectory("Logs");
+            var datetime = DateTime.Now.ToString("yyyy-MM-ddTHH-mm-ss");
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), $"Logs/{datetime}.txt"));
+            var logger = loggerFactory.CreateLogger("FileLogger");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,7 +48,6 @@ namespace WebLab1
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -45,6 +56,15 @@ namespace WebLab1
             app.UseRouting();
 
             app.UseAuthorization();
+            app.Use(async (context, next) =>
+            {
+                var request = context.Request;
+                var fullUrl = $"{request.Scheme}://{request.Host}{request.PathBase}{request.Path}{request.QueryString}";
+                var time = DateTime.Now.ToString("HH:mm:ss");
+                var ip = context.Connection.RemoteIpAddress?.ToString();
+                logger.LogInformation("Processing request {0}", $"{fullUrl}, time: {time}, ip: {ip} ");
+                await next.Invoke();
+            });
 
             app.UseEndpoints(endpoints =>
             {
